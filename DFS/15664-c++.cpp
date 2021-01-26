f8dd4d1c7d093bffcc53f86ca116fdb53808061d
배우진 <baewoojin@baeujin-ui-MacBookPro.local>
2021-01-26 16:02:39 +0900

    15664 is solved by c++

--- DFS/15664-c++.cpp
+++ DFS/15664-c++.cpp
@@ -1,11 +1,11 @@
-//N과 M(9)(15663)
+//N과 M(10)(15664)
 //dfs 재귀로 해결한다.
 //입력되는 N개의 수 중 M개를 고르고 출력한다.
 //재귀 함수의 limit == M이면 각 수열을 출력한다.
 //skill.1 - 입력 받은 수를 정렬해야 한다.
 //skill.2 - 수열과 수열의 요소가 중복될 수 없다.
-//skill.3 - parameter 로 limit 만을 사용한다.
-//skill.4 - set 즉 집합을 사용해서 삽입시 중복을 방지한다.
+//skill.3 - set 즉 집합을 사용해서 삽입시 중복을 방지한다.
+//skill.4 - 수열은 비내림차순 이어야 하기 때문에 j + 1 로 재귀 함수를 호출해야 한다.
 
 #include <iostream>
 #include <algorithm>
@@ -25,7 +25,7 @@ int visited[MAX];
 vector<int> num;
 set<vector<int>> canbe;
 
-void dfs(int limit){
+void dfs(int index, int limit){
     if (limit == M) {
         vector<int> tmp;
         for (int i = 0; i < M; i++) {
@@ -37,11 +37,11 @@ void dfs(int limit){
         tmp.clear();
         return;
     }
-    for (int j = 0; j < N; j++) {
+    for (int j = index; j < N; j++) {
         if (visited[j] == 0) {
             temp[limit] = num.at(j);
             visited[j] = 1;
-            dfs(limit + 1);
+            dfs(j + 1, limit + 1);
             temp[limit] = 0;
             visited[j] = 0;
         }
@@ -56,7 +56,7 @@ int main(void){
         cin >> num.at(i);
     }
     sort(num.begin(), num.end());
-    dfs(0);
+    dfs(0, 0);
     
     for (auto e : canbe) {
         for (int i = 0; i < e.size(); i++) {
